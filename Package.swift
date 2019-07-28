ddd1b2014ee30ec90f30580af8f8373ba442b1ff
omochimetaru <omochi.metaru@gmail.com>
2019-07-28 17:33:36 +0900

    LLVM8

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "MinSwift",
+    platforms: [.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,9 +15,11 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-syntax.git",
                  .revision("swift-DEVELOPMENT-SNAPSHOT-2019-07-10-m")),
         .package(url: "https://github.com/llvm-swift/LLVMSwift.git",
-                 .upToNextMinor(from: "0.4.0")),
+                 .upToNextMinor(from: "0.6.0")),
+        
+        // LLVMSwiftと同じ記述にする
         .package(url: "https://github.com/llvm-swift/FileCheck.git",
-                 .upToNextMinor(from: "0.0.3"))
+                 from: "0.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
